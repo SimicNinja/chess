fbefc31978c68a9ac4d6259f67a9f13b2e8860c1
Owen Werts <owen.werts@gmail.com>
2025-01-15 21:29:10 -0700

    Reformatting

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -6,24 +6,28 @@ package chess;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessPosition {
+public class ChessPosition
+{
 
-    public ChessPosition(int row, int col) {
-    }
+	public ChessPosition(int row, int col)
+	{
+	}
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
-    public int getRow() {
-        throw new RuntimeException("Not implemented");
-    }
+	/**
+	 * @return which row this position is in
+	 * 1 codes for the bottom row
+	 */
+	public int getRow()
+	{
+		throw new RuntimeException("Not implemented");
+	}
 
-    /**
-     * @return which column this position is in
-     * 1 codes for the left row
-     */
-    public int getColumn() {
-        throw new RuntimeException("Not implemented");
-    }
+	/**
+	 * @return which column this position is in
+	 * 1 codes for the left row
+	 */
+	public int getColumn()
+	{
+		throw new RuntimeException("Not implemented");
+	}
 }
