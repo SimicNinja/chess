26cc12bc097d001a9f2918fef9005abef47da238
Owen Werts <owen.werts@gmail.com>
2025-04-16 23:54:40 -0600

    Removed Server intiation helps for testing from Client Main.java.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,5 @@
 import client.REPL;
-import server.Server;
+//import server.Server;
 import serverfacade.ResponseException;
 import serverfacade.ServerFacade;
 
@@ -8,23 +8,23 @@ public class Main
 	public static void main(String[] args) throws ResponseException
 	{
 		String serverURL = "http://localhost:8080";
-		startServer();
+//		startServer();
 
 		new REPL(serverURL).run();
 
-		clearServer(serverURL);
+//		clearServer(serverURL);
 	}
 
-	private static void startServer()
-	{
-		Server server = new Server();
-		var port = server.run(8080);
-		System.out.println("Started test HTTP server on " + port);
-	}
-
-	private static void clearServer(String serverURL) throws ResponseException
-	{
-		ServerFacade facade = new ServerFacade(serverURL);
-		facade.clear();
-	}
+//	private static void startServer()
+//	{
+//		Server server = new Server();
+//		var port = server.run(8080);
+//		System.out.println("Started test HTTP server on " + port);
+//	}
+//
+//	private static void clearServer(String serverURL) throws ResponseException
+//	{
+//		ServerFacade facade = new ServerFacade(serverURL);
+//		facade.clear();
+//	}
 }
